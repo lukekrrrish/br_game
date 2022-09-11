5e9a61fe292684454adbd95192335e6f6873d1eb
lukekrrish <lukekrrish@gmail.com>
2022-09-10 23:22:30 -0500

    Start

---
+++ main.py
@@ -0,0 +1,52 @@
+from mains import *
+
+
+def start():
+    global o_f
+    choice = int(
+        input("\n1 = End Game\nClick Anything other than 1 to start the game.\n"))
+    if choice == 1:
+        o_f = False
+    else:
+        yes_choice = int(
+            input("\n1 = Back\nAnything else = Game\n"))
+        if yes_choice == 1:
+            start()
+        else:
+            gun_choice = int(
+                input(f"\nWhich Gun Do u want to use?\n1 = Sniper\n2 = AR\n3 = Gernade\n4 = ShotGun\n"))
+            if gun_choice == 1:
+                sniper_choice = int(
+                    input(f"\nWhich Sniper Do u want to use?\n1 = Rifle\n2 = bolt\n"))
+                if sniper_choice == 1:
+                    rifle_color_choice = int(
+                        input(f"\nWhich Rifle Color Do u want to use?\n1 = Gray\n2 = Green\n3 = Blue\n4 = purple\n5 = red\n6 = gold\n"))
+            if gun_choice == 2:
+                sniper_choice = int(
+                    input(f"\nWhich AR Do u want to use?\n1 = \n2 = \n3 = "))
+                if sniper_choice == 1:
+                    rifle_color_choice = int(
+                        input(f"\nWhich AR Color Do u want to use?\n1 = Gray\n2 = Green\n3 = Blue\n4 = purple\n5 = red\n6 = gold\n"))
+            if gun_choice == 2:
+                sniper_choice = int(
+                    input(f"\nWhich Gernade Do u want to use?\n1 = \n2 = \n3 = "))
+                if sniper_choice == 1:
+                    rifle_color_choice = int(
+                        input(f"\nWhich  Color Do u want to use?\n1 = Gray\n2 = Green\n3 = Blue\n4 = purple\n5 = red\n6 = gold\n"))
+            if gun_choice == 2:
+                sniper_choice = int(
+                    input(f"\nWhich AR Do u want to use?\n1 = \n2 = \n3 = "))
+                if sniper_choice == 1:
+                    rifle_color_choice = int(
+                        input(f"\nWhich  Color Do u want to use?\n1 = Gray\n2 = Green\n3 = Blue\n4 = purple\n5 = red\n6 = gold\n"))
+            if gun_choice == 2:
+                sniper_choice = int(
+                    input(f"\nWhich AR Do u want to use?\n1 = \n2 = \n3 = "))
+                if sniper_choice == 1:
+                    rifle_color_choice = int(
+                        input(f"\nWhich  Color Do u want to use?\n1 = Gray\n2 = Green\n3 = Blue\n4 = purple\n5 = red\n6 = gold\n"))
+            
+
+
+while o_f:
+    start()
---
+++ mains/vars.py
@@ -0,0 +1,88 @@
+from .imports import *
+
+
+
+o_f = True
+57
+
+class guns():
+    class snipers():
+        class gray():
+            class rifle():
+                dmg = 50
+            class bolt():
+                dmg = 60
+        class green():
+            class rifle():
+                dmg = 56
+            class bolt():
+                dmg = 68
+        class blue():
+            class rifle():
+                dmg = 60
+            class bolt():
+                dmg = 75
+        class purple():
+            class rifle():
+                dmg = 68
+            class bolt():
+                dmg = 84
+        class gold():
+            class rifle():
+                dmg = 75
+            class bolt():
+                dmg = 92
+    class AR():
+        class gray():
+            pass
+        class green():
+            pass
+        class blue():
+            pass
+        class purple():
+            pass
+        class gold():
+            pass
+    class SMG():
+        class gray():
+            pass
+        class green():
+            pass
+        class blue():
+            pass
+        class purple():
+            pass
+        class gold():
+            pass
+    class Gernade():
+        class gray():
+            pass
+        class green():
+            pass
+        class blue():
+            pass
+        class purple():
+            pass
+        class gold():
+            pass
+    class shotgun():
+        class gray():
+            pass
+        class green():
+            class shotgun():
+                pass
+            class musket():
+                dmg = 57
+        class blue():
+            pass
+        class purple():
+            pass
+        class gold():
+            pass
+    class mythic():
+        class heavy():
+            pass
+        class drum():
+            pass
+        class lmg():
+            pass
